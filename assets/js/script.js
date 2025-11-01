d5f2893e8de1abd816bb35ff5810e3ca186536b6
Timmy W <137299906+ThatMayBeTheCase@users.noreply.github.com>
2025-11-01 01:56:07 +0100

    Chore(scaffold): Foundation / structure (#1)
    * html structure
    
    * added base CSS.
    
    * DOM listener added.

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('bookmark-form');
+    const list = document.getElementById('bookmark-list');
+    const error = document.getElementById('form-error');
+    const empty = document.getElementById('empty-state');
+
+    // hookar till senare functions.
+
+});
