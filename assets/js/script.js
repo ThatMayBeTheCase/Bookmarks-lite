75f0723c3af005d6d642b0ddde9f4cc01c35f736
ThatMayBeTheCase <timmy_wramborg97@hotmail.com>
2025-11-03 14:31:44 +0100

    added render function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,38 @@ document.addEventListener('DOMContentLoaded', () => {
     const error = document.getElementById('form-error');
     const empty = document.getElementById('empty-state');
 
-    // vår databas i minnet.
+    // "database" in memory.
     const bookmarks = [];
 
-    function normalizerUrl(value) {
+    function normalizeUrl(value) {
         if (!value) return null;
         const withProto = /^https?:\/\//i.test(value) ? value : `https://${value}`;
         try {
             const url = new URL(withProto);
             return url.toString();
         }
-        catch {
+            catch {
             return null;
+        }
+    }
+
+    function render() {
+        // empty list.
+        list.innterHTML = '';
+
+        if (bookmarks.length === 0) {
+            empty.style.display = 'block';
+            return;
+        }
+        empty.style.display = 'none';
+
+        for (const bm of bookmarks) {
+            const li = document.createElement('li');
+            li.innerHTML = `
+            <strong>${bm.title}</strong><br>
+            <span>${bm.url}</span>
+            `;
+            list.appendChild(li);
         }
     }
 
